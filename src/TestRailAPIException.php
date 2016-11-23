6dc40484004671fb99fc59e3c88f50787e88f47d
Krzysztof Daniel <krzysztof.daniel@creativestyle.pl>
2016-11-23 18:56:18 +0100

    file split to Classes and composer.json

---
+++ src/TestRailAPIException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * TestRail API binding for PHP (API v2, available since TestRail 3.0)
+ *
+ * Learn more:
+ *
+ * http://docs.gurock.com/testrail-api2/start
+ * http://docs.gurock.com/testrail-api2/accessing
+ *
+ * Copyright Gurock Software GmbH. See license.md for details.
+ */
+
+class TestRailAPIException extends Exception
+{
+}
